771bb2164f632b9e0c6a40253c8bbbf617f77cd7
Mrbysco <Mrbysco@users.noreply.github.com>
2024-07-07 22:12:30 +0200

    Update NeoForge support

--- neoforge/src/main/java/com/mrbysco/ghastcow/registry/ModSetup.java
+++ neoforge/src/main/java/com/mrbysco/ghastcow/registry/ModSetup.java
@@ -5,12 +5,12 @@ import com.mrbysco.ghastcow.registration.ModEntities;
 import net.minecraft.world.entity.SpawnPlacementTypes;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
-import net.neoforged.neoforge.event.entity.SpawnPlacementRegisterEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 public class ModSetup {
 
-	public static void registerSpawnPlacements(final SpawnPlacementRegisterEvent event) {
-		event.register(ModEntities.GHAST_COW.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, GhastCow::canSpawnHere, SpawnPlacementRegisterEvent.Operation.OR);
+	public static void registerSpawnPlacements(final RegisterSpawnPlacementsEvent event) {
+		event.register(ModEntities.GHAST_COW.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, GhastCow::canSpawnHere, RegisterSpawnPlacementsEvent.Operation.OR);
 	}
 
 	public static void registerEntityAttributes(EntityAttributeCreationEvent event) {
